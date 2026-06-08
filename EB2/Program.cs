2f5050a3a193e87b4f30195250ed9a36dd4b3fb7
Ricardo <rickypeval9@gmail.com>
2026-06-08 08:42:28 -0600

    Clase 2: Implementación de Stack vs Heap

---
+++ EB2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void CambiarValor(int x)
+    {
+        x = 100;
+        Console.WriteLine("Dentro de CambiarValor: " + x);
+    }
+
+    static void CambiarReferencia(int[] arr)
+    {
+        arr[0] = 100;
+        Console.WriteLine("Dentro de CambiarReferencia: " + arr[0]);
+    }
+
+    static void Main()
+    {
+        int numero = 5;
+        int[] arreglo = { 1, 2, 3 };
+
+        Console.WriteLine("Antes de CambiarValor: " + numero);
+        CambiarValor(numero);
+        Console.WriteLine("Despues de CambiarValor: " + numero);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Antes de CambiarReferencia: " + arreglo[0]);
+        CambiarReferencia(arreglo);
+        Console.WriteLine("Despues de CambiarReferencia: " + arreglo[0]);
+    }
+}
